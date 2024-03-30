d5b213cb6990c31bb222c92b5c69a3bceed00c35
siva <siva@google.com>
2024-03-30 18:17:00 +0530

    addef first code

---
+++ src/com/i27academy/builds/Calculator.groovy
@@ -0,0 +1,11 @@
+package com.i27academy.builds
+
+class Calculator {
+    def jenkins
+
+    Caluclator(jenkins) {
+        this.jenkins = jenkins
+    }
+
+
+}
---
+++ src/com/i27academy/builds/Docker.groovy
@@ -0,0 +1,25 @@
+package com.i27academy.builds
+
+class Docker {
+    def jenkins
+    Docker(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    // Addition method
+    def add(firstNumber, secondNumber) { //add(1,2)
+        // logic
+        return firstNumber+secondNumber
+    }
+    
+    // Application Build
+    def buildApp() {
+        jenkins.sh """#!/bin/bash
+        echo "Building the Eureka Application
+        sh "mvn clean package -DskipTests=true"
+        """
+    }
+    
+}
+
+
