ecefc6684e37d49a28a31859ac3986ffe440bb38
siva <siva@google.com>
2024-03-30 18:33:56 +0530

    removed sh

--- src/com/i27academy/builds/Docker.groovy
+++ src/com/i27academy/builds/Docker.groovy
@@ -16,7 +16,7 @@ class Docker {
     def buildApp() {
         jenkins.sh """#!/bin/bash
         echo "Building the Shared Library Eureka Application"
-        sh "mvn clean package -DskipTests=true"
+        mvn clean package -DskipTests=true
         """
     }
     
